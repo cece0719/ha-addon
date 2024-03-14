99057b4b650a2c9523e2fb81c369eb0e15a3008e
cece0719 <cece0719@naver.com>
2024-03-14 11:37:59 +0900

    d

--- test/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo start
-node /share/index.js
-
-#while true; do echo "still live"; sleep 1800; done
--- theshop/run.py
+++ theshop/run.py
@@ -12,7 +12,7 @@ import re
 logger = logging.getLogger(__name__)
 
 
-class SDSSerial:
+class KSX4506_Serial:
     def __init__(self):
         self._ser = serial.Serial()
         self._ser.port = "/dev/ttyUSB0"
@@ -24,34 +24,35 @@ class SDSSerial:
         self._ser.close()
         self._ser.open()
 
-        self._pending_recv = 0
-
         # 시리얼에 뭐가 떠다니는지 확인
         self.set_timeout(5.0)
-        data = self._recv_raw(1)
+        data = self._ser.read(1)
         self.set_timeout(None)
         if not data:
             logger.critical("no active packet at this serial port!")
 
-    def _recv_raw(self, count=1):
-        return self._ser.read(count)
-
-    def recv(self, count=1):
-        # serial은 pending count만 업데이트
-        self._pending_recv = max(self._pending_recv - count, 0)
-        return self._recv_raw(count)
+    def recvRaw(self):
+        header = self._ser.read(1)
+        deviceId = self._ser.read(1)
+        deviceSubId = self._ser.read(1)
+        commandType = self._ser.read(1)
+        length = self._ser.read(1)
+        data = self._ser.read(length)
+        xorSum = self._ser.read(1)
+        addSum = self._ser.read(1)
+        logs=[]
+        logs.append(header)
+        logs.append(deviceId)
+        logs.append(deviceSubId)
+        logs.append(commandType)
+        logs.append(length)
+        logs.append(data)
+        logs.append(xorSum)
+        logs.append(addSum)
+        return logs
 
     def send(self, a):
         self._ser.write(a)
-
-    def set_pending_recv(self):
-        self._pending_recv = self._ser.in_waiting
-
-    def check_pending_recv(self):
-        return self._pending_recv
-
-    def check_in_waiting(self):
-        return self._ser.in_waiting
 
     def set_timeout(self, a):
         self._ser.timeout = a
@@ -71,20 +72,13 @@ def dump_loop():
     logs = []
     while True:
         try:
-            data = conn.recv(conn.check_in_waiting())
+            data = conn.recvRaw()
         except:
             continue
 
         if data:
             logs = []
             for b in data:
-                #               if b == 0xA1 or len(logs) > 500:
-                #                   logger.info("".join(logs))
-                #                   logs = ["{:02X}".format(b)]
-                #               elif b <= 0xA0: logs.append(   "{:02X}".format(b))
-                #               elif b == 0xFF: logs.append(   "{:02X}".format(b))
-                #               elif b == 0xB0: logs.append( ": {:02X}".format(b))
-                #               else:           logs.append(",  {:02X}".format(b))
                 logs.append(" {:02X}".format(b))
             logger.info("".join(logs))
         time.sleep(0.01)
@@ -101,6 +95,6 @@ if __name__ == "__main__":
     init_logger()
 
     logger.info("initialize serial...")
-    conn = SDSSerial()
+    conn = KSX4506_Serial()
 
     dump_loop()
