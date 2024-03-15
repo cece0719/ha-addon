9b357fc5053328b84f4a7c7dfb0730a52a6756cd
cece0719 <cece0719@naver.com>
2024-03-15 18:17:12 +0900

    sdfsdf

--- theshop/py/device_light.py
+++ theshop/py/device_light.py
@@ -10,7 +10,7 @@ class DeviceLight:
         ""
 
     def receive_serial(self, data):
-        if data.startwith(b'\xf7\x0e\x1f\x81'):
+        if data.startswith(b'\xf7\x0e\x1f\x81'):
             if data[4 + self.number] == 1:
                 logging.info("light" + str(self.number) + "status on")
                 self.status = True
