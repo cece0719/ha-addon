a5892134eb282d4b790e32bd093a20685b4c7895
cece0719 <cece0719@naver.com>
2024-03-16 16:19:10 +0900

    sdfsdfg

--- theshop/py/device_elevator.py
+++ theshop/py/device_elevator.py
@@ -6,9 +6,19 @@ class DeviceElevator:
         self.mqtt = mqtt
         self.serial = serial
         mqtt.add_device(self)
-        # serial.add_device(self)
+        self.publishes = [
+            {
+                "topic": "homeassistant/button/cece0719/button_elevator/config",
+                "payload": {
+                    "unique_id": "button_elevator",
+                    "command_topic": "cece0719/button_elevator/command",
+                    # "state_topic": "cece0719/button_elevator/state",
+                    # "payload_on": 'ON',
+                    # "payload_off": 'OFF',
+                }
+            }
+        ]
 
     def receive_mqtt(self, topic, payload):
-        if topic == "cece0719/elevator":
-            logging.info("getElevatorCall")
-            # self.serial.send(b'\xf7\x33\x01\x81\x03\x00\x20\x00')
+        if topic == "cece0719/button_elevator/command" and payload == "PRESS":
+            self.serial.send(b'\x33\x01\x81\x03\x00\x20\x00')
