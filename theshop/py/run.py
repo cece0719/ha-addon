2a9d803d3fe322a91f6600fd74ae7c785988fb32
박명철 <cece0719@naver.com>
2024-09-06 17:06:34 +0900

    test

--- theshop/py/run.py
+++ theshop/py/run.py
@@ -51,24 +51,23 @@ if __name__ == "__main__":
 
     class Handler(http.server.SimpleHTTPRequestHandler):
         def do_POST(self):
-            pass
-            # content_len = int(self.headers.get("Content-Length"))
-            # body = self.rfile.read(content_len)
-            # body_json = json.loads(body)
-            #
-            # logging.info(body_json)
-            #
-            # header_name = body_json["header"]["name"]
-            # if header_name == "DiscoverAppliancesRequest":
-            #     response = clova.discover(body_json)
-            # else:
-            #     response = clova.action(body_json)
-            #
-            # logging.info(response)
-            #
-            # self.send_response(HTTPStatus.OK)
-            # self.end_headers()
-            # self.wfile.write(response.encode("utf8"))
+            content_len = int(self.headers.get("Content-Length"))
+            body = self.rfile.read(content_len)
+            body_json = json.loads(body)
+
+            logging.info(body_json)
+
+            header_name = body_json["header"]["name"]
+            if header_name == "DiscoverAppliancesRequest":
+                response = clova.discover(body_json)
+            else:
+                response = clova.action(body_json)
+
+            logging.info(response)
+
+            self.send_response(HTTPStatus.OK)
+            self.end_headers()
+            self.wfile.write(response.encode("utf8"))
 
 
     logging.info("try http start")
