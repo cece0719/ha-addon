dd33283cd0fc731b62ed6fa3db3b5edcd8a13546
박명철 <cece0719@naver.com>
2024-09-13 17:25:49 +0900

    test

--- theshop/py/run.py
+++ theshop/py/run.py
@@ -1,14 +1,13 @@
 import json
 import logging
 import sys
-from typing import List, Callable
+from typing import List
 
 from device.device import Device
 from device.device_elevator import DeviceElevator
 from device.device_gas import DeviceGas
 from device.device_light import DeviceLight
 from device.device_light_total import DeviceLightTotal
-from device.device_mqtt import DeviceMqtt
 from theshopclova import TheShopClova
 from theshopmqtt import TheShopMQTT
 from theshopserial import TheShopSerial
@@ -26,16 +25,13 @@ if __name__ == "__main__":
     serial = TheShopSerial(option)
     clova = TheShopClova(option)
 
-    mqtt_publish: Callable[[DeviceMqtt, str, str], None] = mqtt.publish
-    serial_send: Callable[[bytes], None] = serial.send
-
     devices: List[Device] = [
-        DeviceLight(1, "거실1", ["거실"], mqtt_publish, serial_send),
-        DeviceLight(2, "거실2", ["거실"], mqtt_publish, serial_send),
-        DeviceLight(3, "복도", ["복도"], mqtt_publish, serial_send),
-        DeviceLightTotal(mqtt_publish, serial_send),
-        DeviceGas(mqtt_publish, serial_send),
-        DeviceElevator(serial_send),
+        DeviceLight(1, "거실1", ["거실"], mqtt.publish, serial.send),
+        DeviceLight(2, "거실2", ["거실"], mqtt.publish, serial.send),
+        DeviceLight(3, "복도", ["복도"], mqtt.publish, serial.send),
+        DeviceLightTotal(mqtt.publish, serial.send),
+        DeviceGas(mqtt.publish, serial.send),
+        DeviceElevator(serial.send),
     ]
 
     mqtt.add_devices(devices)
