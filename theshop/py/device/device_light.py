48a728d7b4196a1decaf6a533632b851c0dfb447
박명철 <cece0719@naver.com>
2025-04-10 01:16:16 +0900

    test

--- theshop/py/device/device_light.py
+++ theshop/py/device/device_light.py
@@ -41,10 +41,10 @@ class DeviceLight(DeviceMqtt, DeviceSerial, DeviceClova):
         return "light"
 
     def turn_on(self):
-        self.serial_send(b'\x0E' + (self.number + 16).to_bytes(1, "big") + b'\x41\x03\x01\x01\x00')
+        self.serial_send(b'\x0E' + (self.number + 16).to_bytes(1, "big") + b'\x41\x03' + self.sub_number.to_bytes(1, "big") + b'\x01\x00')
 
     def turn_off(self):
-        self.serial_send(b'\x0E' + (self.number + 16).to_bytes(1, "big") + b'\x41\x03\x00\x00\x00')
+        self.serial_send(b'\x0E' + (self.number + 16).to_bytes(1, "big") + b'\x41\x03' + self.sub_number.to_bytes(1, "big") + b'\x00\x00')
 
     def receive_serial(self, data: bytes):
         if data.startswith(b'\xf7\x0e' + (self.number + 16).to_bytes(1, "big") + b'\x81'):
