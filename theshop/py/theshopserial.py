157da6b406cf78db011081c5cc3301201ca855ff
박명철 <cece0719@naver.com>
2025-04-11 11:34:15 +0900

    test

--- theshop/py/theshopserial.py
+++ theshop/py/theshopserial.py
@@ -70,7 +70,25 @@ class TheShopSerial:
         def listen():
             while True:
                 data = self.read_raw()
-                logging.debug(data.hex(" "))
+                data_hex = data.hex(" ")
+                if data_hex.startswith("f7 0e"):
+                    logging.debug(data_hex) #조명
+                elif data_hex.startswith("f7 12"):
+                    logging.debug(data_hex) #가스
+                elif data_hex.startswith("f7 30"):
+                    logging.debug(data_hex) #검침
+                    #https://cafe.naver.com/koreassistant/17710?art=ZXh0ZXJuYWwtc2VydmljZS1uYXZlci1zZWFyY2gtY2FmZS1wcg.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjYWZlVHlwZSI6IkNBRkVfVVJMIiwiY2FmZVVybCI6ImtvcmVhc3Npc3RhbnQiLCJhcnRpY2xlSWQiOjE3NzEwLCJpc3N1ZWRBdCI6MTc0NDMzODUyODQ5M30.L4UbCPE1KCSMjArkE6hgvZhjXi-HiR6mNPz0AylebRo
+                elif data_hex.startswith("f7 33"):
+                    logging.debug(data_hex) #일괄차단기
+                elif data_hex.startswith("f7 36"):
+                    logging.debug(data_hex) #난방
+                elif data_hex.startswith("f7 39"):
+                    logging.debug(data_hex) #POWEROUTLET?
+                elif data_hex.startswith("f7 40"):
+                    logging.debug(data_hex) #문열기관련!
+                    #https://cafe.naver.com/koreassistant?iframe_url_utf8=%2FArticleRead.nhn%3Fclubid%3D29860180%26articleid%3D15627 참고!
+                else:
+                    logging.info(data_hex)
                 for device in self.devices.values():
                     device.receive_serial(data)
 
