c6d7a37ce155bf97006eed2c7abe0ab4302a327c
박명철 <cece0719@naver.com>
2025-04-30 19:53:10 +0900

    test

--- theshop/py/device/device_light_total.py
+++ theshop/py/device/device_light_total.py
@@ -39,7 +39,11 @@ class DeviceLightTotal(DeviceMqtt, DeviceSerial, DeviceClova):
 
     def receive_serial(self, data: bytes):
         if data.startswith(b'\xf7\x33\x01\x81'):
-            self.status = (data[6] != 4)
+            if data[6] == 4:
+                self.status = True
+            if data[6] == 0:
+                self.status = False
+
             if self.status:
                 self.mqtt_publish(self, "state", "ON")
             else:
